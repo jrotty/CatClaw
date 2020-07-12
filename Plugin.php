dd8b88cebf1e4e5234b19838b6e8e52be8f57e8a
jrotty <bssf@qq.com>
2020-07-12 10:42:27 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -6,7 +6,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * 
  * @package CatClaw
  * @author jrotty
- * @version 1.1.1
+ * @version 1.1.2
  * @link https://qqdie.com
  */
 class CatClaw_Plugin implements Typecho_Plugin_Interface
