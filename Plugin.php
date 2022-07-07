b8b82d77b058040ba90804dafa4b53e346162906
jrotty <bssf@qq.com>
2022-07-07 12:20:01 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -6,8 +6,8 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * 
  * @package CatClaw
  * @author jrotty
- * @version 1.7.0
- * @link https://zezeshe.com/archives/typecho-CatClaw.html
+ * @version 1.7.1
+ * @link https://blog.zezeshe.com/
  */
 class CatClaw_Plugin implements Typecho_Plugin_Interface
 {
